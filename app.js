c8246659f4cd437d1674d752184f132a4c2e1931
Daniel <duquedk1@gmail.com>
2021-09-14 23:33:46 -0500

    updating app.js to capture values

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 
 window.onload = () =>{
-
+    const form = document.getElementById('todo-form')
+    form.onsubmit = (e) => {
+        e.preventDefault()
+        const todo = document.getElementById('todo')
+        const todoText = todo.value
+        todo.value = ''
+        console.log(todoText)
+    }
 }
 
 
