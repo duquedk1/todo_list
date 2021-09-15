4841e2a5d9e7a13ac344e453e8458752064c63b3
Daniel <duquedk1@gmail.com>
2021-09-14 23:47:46 -0500

    updating app.js simplifying with map

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ window.onload = () =>{
         todo.value = ''
         todos.push(todoText)
         const todoList =document.getElementById('todo-list')
-        todoList.innerHTML =''
+        /* todoList.innerHTML =''
         for(let i = 0; i < todos.length; i++){
             todoList.innerHTML += '<li>' + todos[i] + '</li>'
-        }
-        
+        }*/
+        const todosTemplate = todos.map(t =>'<li>' + t + '</li>')
+        todoList.innerHTML = todosTemplate.join('')
     }
 }
 
